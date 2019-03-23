fac480e3e7cb8cd393478932b5c73ea4704b546d
Ryan Anthony Hess <ryanhess1990@gmail.com>
2019-03-23 18:17:33 -0400

    03/23/19
    small change

--- customjs/mycustomscript.js
+++ customjs/mycustomscript.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     $('#buttonevent').on('click', function () {
         alert('Unfortunately this feature is currently down for maintenance and will return better than ever shortly. We apologize for any inconvenience this may cause and wish you a wonderful day!')
     });
