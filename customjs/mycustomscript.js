65c6658ffa285bc192938233003c503cc84a438d
Ryan Anthony Hess <37939083+ryananthonyhess@users.noreply.github.com>
2019-03-27 10:18:55 -0400

    changes

--- customjs/mycustomscript.js
+++ customjs/mycustomscript.js
@@ -4,3 +4,11 @@ $(document).ready(function() {
         alert('Unfortunately this feature is currently down for maintenance and will return better than ever shortly. We apologize for any inconvenience this may cause and wish you a wonderful day!')
     });
 });
+
+//for the email
+$(document).ready(function(){
+    $('.buttonemail').on('click',function(){
+       window.location.href = "mailto:info@metrodefender.com?subject=Subject&body=message%20goes%20here"; 
+    });
+});
+}
